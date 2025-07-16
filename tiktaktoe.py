20af24e1773beda3a15966f30f26ace853293287
Xavier Bester <xavierbester@gmail.com>
2025-07-15 18:56:24 -0600

    might have finished it for the most part but not too sure yet

--- tiktaktoe.py
+++ tiktaktoe.py
@@ -24,10 +24,53 @@ def win(player):
         return False
     
 def winInColumn(player):
+    win = [player]*3
+    window = ['.']*3
+
+    for column in range(3):
+        window[0] = map[0][column]
+        window[1] = map[1][column]
+        window[2] = map[2][column]
+        if window == win:
+            return True
+    return False
+
 
 def winInRow(player):
+    win = [player] *3
+    window = ['.']*3
+
+    for row in range(3):
+        window[0] = map[row][0]
+        window[1] = map[row][1]
+        window[2] = map[row][2]
+
+        if window == win:
+            return True
+    
+    return False
+
 
 def winInDiagonals(player):
+    win = [player]*3
+    window = ['.']*3
+
+    for i in range(3):
+        window = map[i][i]
+    if win == window:
+        return True
+    
+    for i in range(3):
+        window = map[i][2 - i]
+    if win == window:
+        return True
+    
+    return False
+
 
 def isTie():
-
+    for row in map:
+        for column in row:
+            if map[row][column] == '.':
+                return False
+    return True
